b40febe0d7cbd4d00bdefa9d16076d15e9356b18
AkshayRam19 <160832588+AkshayRam19@users.noreply.github.com>
2024-04-15 15:36:13 +0530

    Update main.java

--- main.java
+++ main.java
@@ -1,12 +1,6 @@
 class main {
 
   public static void main(String[] args) {
-    
-    int first = 10;
-    int second = 20;
-
-    // add two numbers
-    int sum = first + second;
-    System.out.println(first + " + " + second + " = "  + sum);
+    system.out.println("HelloWorld!")
   }
 }
